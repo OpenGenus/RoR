90699d2271ddf5a6d0ea8b92eef947319bc7b188
unknown <sharonkessio6@gmail.com>
2023-02-13 09:22:16 +0300

    chnage routes order

--- blogApp/config/routes.rb
+++ blogApp/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "timelines#index"
   get 'timelines', to: 'timelines#index'
+  get '/timelines/new', to: 'timelines#new', as: 'new_timeline'
   get '/timelines/:id', to: 'timelines#destroy'
   post 'timelines', to: 'timelines#create'
   delete '/timelines/:id', to: 'timelines#destroy', as: 'timeline'
-  get '/timelines/new', to: 'timelines#new', as: 'new_timeline'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
